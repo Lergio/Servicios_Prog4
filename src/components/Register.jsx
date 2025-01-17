85146d59695bf857a5b9068be491d4a7cc26b596
Colo143 <coloexe143@gmail.com>
2025-01-17 10:44:59 -0300

    Actualización Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,25 +19,32 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+  
     try {
       const response = await axios.post('http://181.199.159.26:8080/api/auth/register/', formData);
+      const { access, refresh } = response.data;
+      localStorage.setItem('accessToken', access);
+      localStorage.setItem('refreshToken', refresh);
+      
       console.log(response.data);
-      navigate('/profile');
+
+      // Redirigir al dashboard si el login es exitoso
+      navigate('/login');
     } catch (error) {
-      console.error(error);
+      console.error('Error al iniciar sesión:', error);
     }
   };
 
-  return (
+  {return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <input type="text" name="username" placeholder="Name" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded" />
+          <input type="text" name="username" placeholder="Username" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded" />
           <input type="email" name="email" placeholder="Email" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded" />
           <input type="password" name="password" placeholder="Password" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded" />
-          <input type="text" name="first_name" placeholder="First_name" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded" />
-          <input type="text" name="last_name" placeholder="Last_name" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded" />
+          <input type="text" name="first_name" placeholder="First_name" onChange={handleChange} className="w-full p-2 border border-gray-300 rounded" />
+          <input type="text" name="last_name" placeholder="Last_name" onChange={handleChange} className="w-full p-2 border border-gray-300 rounded" />
           {/* <select name="rol" onChange={handleChange} required className="w-full p-2 border border-gray-300 rounded">
             <option value="">Select Role</option>
             <option value="role1">Provider</option>
@@ -48,7 +55,7 @@ const Register = () => {
         </form>
       </div>
     </div>
-  );
+  );}
 };
 
 export default Register;
